26b1c966fe6cd34f2c76e80f730a6e704d3848fe
imrayh <raykah.silva@gmail.com>
2022-08-13 14:29:49 -0300

    update files to implements CI/CD

--- dockerfile
+++ dockerfile
@@ -2,11 +2,3 @@ FROM openjdk:11
 EXPOSE 8080
 ADD target/rayhbank.jar rayhbank.jar
 ENTRYPOINT ["java", "-jar", "rayhbank.jar"]
-
-#
-#FROM openjdk:11-jre
-#COPY --from=maven /target/RayhBank-0.0.1-SNAPSHOT.jar /app/
-#WORKDIR /app
-#ENTRYPOINT = ["java", "-jar", "RayhBank-0.0.1-SNAPSHOT.jar"]
-
-
--- src/main/java/ifg/edu/rayhbank/RayhBankApplication.java
+++ src/main/java/ifg/edu/rayhbank/RayhBankApplication.java
@@ -20,7 +20,7 @@ public class RayhBankApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200",""));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:8081",""));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
