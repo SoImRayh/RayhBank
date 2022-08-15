ae5ca37d4b8170d144dfc4e4c14a2c73b9e6a62b
imrayh <raykah.silva@gmail.com>
2022-08-15 17:57:08 -0300

    implementação de JJwt

--- src/main/java/ifg/edu/rayhbank/RayhBankApplication.java
+++ src/main/java/ifg/edu/rayhbank/RayhBankApplication.java
@@ -20,7 +20,7 @@ public class RayhBankApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:8081","http://localhost:80","http://localhost",
+        corsConfiguration.setAllowedOrigins(Arrays.asList(
                 "http://25.69.25.243:4200","http://localhost:4200" , "http://146.148.66.236"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
